941a367b9a4aefed2e33b6872e43eae020d631c2
AMIT PANDEY <amitpaypal123121@gmail.com>
2019-06-27 06:52:06 +0000

    Add files via upload

--- stdplugins/insta1.py
+++ stdplugins/insta1.py
@@ -15,5 +15,8 @@ async def _(event):
     response_api = requests.get(sample_url.format(input_str, apikey)).json()
     if response_api["cod"] == 200:
         await event.edit(input_str + "\n `" +  "Username:" + json.dumps(response_api["username"]) + "`\n" + "Full Name:" +  json.dumps(response_api["full_name"]) + "`\n" + "Followers:" + json.dumps(response_api["followers"]) + "`\n"  + "Following:" + json.dumps(response_api["following"]) + "`\n" + "Post Count:" +  json.dumps(response_api["post"]) + "`\n" + "Profile Picture Url:" + json.dumps(response_api["profile_pict"]) + "`\n" + "Creator:" + "@hackedyouagain"
+    elif:
+         await event.edit(input_str + "\n `" +  "Username:" + json.dumps(response_api["username"]) + "`\n" + "Full Name:" +  json.dumps(response_api["full_name"]) + "`\n" + "Followers:" + json.dumps(response_api["followers"]) + "`\n"  + "Following:" + json.dumps(response_api["following"]) + "`\n" + "Post Count:" +  json.dumps(response_api["post"]) + "`\n" + "Profile Picture Url:" + json.dumps(response_api["profile_pict"]) + "`\n" + "Creator:" + "@hackedyouagain"
     else:
-        await event.edit(input_str + "\n `" +  "Username:" + json.dumps(response_api["username"]) + "`\n" + "Full Name:" +  json.dumps(response_api["full_name"]) + "`\n" + "Followers:" + json.dumps(response_api["followers"]) + "`\n"  + "Following:" + json.dumps(response_api["following"]) + "`\n" + "Post Count:" +  json.dumps(response_api["post"]) + "`\n" + "Profile Picture Url:" + json.dumps(response_api["profile_pict"]) + "`\n" + "Creator:" + "@hackedyouagain"
+    	print("Hello Didnt work")
+    

34adc10df257e9c4feba6c1b4b34aa5ae442c733
AMIT PANDEY <amitpaypal123121@gmail.com>
2019-06-27 08:08:54 +0000

    Add files via upload

---
+++ stdplugins/insta.py
@@ -0,0 +1,61 @@
+
+import aiohttp
+import time
+from datetime import tzinfo, datetime
+from uniborg.util import admin_cmd
+
+
+
+@borg.on(admin_cmd("ig1 (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    sample_url = "https://rest.farzain.com/api/ig_profile.php?id={}&apikey={}"
+    input_str = event.pattern_match.group(1)
+    async with aiohttp.ClientSession() as session:
+        response_api_zero = await session.get(sample_url.format(input_str, Config.apikey))
+    response_api = await response_api_zero.json()
+    if response_api["cod"] == 200:
+        await event.edit(
+            """**{}**
+**Name:** {}
+**Username:** {}
+**URL:** {}
+**Bio:** {}
+**Followers:** {}
+**Following:** {}
+**Post:** {}
+**Profile Picture:**  {}  {}""".format(
+                input_str,               
+                response_api["info"]["full_name"],
+                response_api["info"]["username"],
+                response_api["info"]["url_bio"],
+                response_api["info"]["bio"],
+                response_api["count"]["followers"],
+                response_api["count"]["following"],
+                response_api["count"]["post"],
+                response_api["info"]["profile_pict"]
+            )
+        )
+    else:
+        await event.edit(  
+            """**{}**
+**Name:** {}
+**Username:** {}
+**URL:** {}
+**Bio:** {}
+**Followers:** {}
+**Following:** {}
+**Post:** {}
+**Profile Picture:**  {}  {}""".format(
+                input_str,               
+                response_api["info"]["full_name"],
+                response_api["info"]["username"],
+                response_api["info"]["url_bio"],
+                response_api["info"]["bio"],
+                response_api["count"]["followers"],
+                response_api["count"]["following"],
+                response_api["count"]["post"],
+                response_api["info"]["profile_pict"]
+            )
+        )
